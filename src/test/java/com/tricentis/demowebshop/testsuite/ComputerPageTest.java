428ff11947ebd3e4791b45ee5211385f0cbf8d1a
kpinalk <yourgithub email>
2024-12-11 01:42:53 +0000

    my project 3

---
+++ src/test/java/com/tricentis/demowebshop/testsuite/ComputerPageTest.java
@@ -0,0 +1,105 @@
+package com.tricentis.demowebshop.testsuite;
+
+import com.tricentis.demowebshop.customlisteners.CustomListeners;
+import com.tricentis.demowebshop.pages.BuildYourOwnComputerPage;
+import com.tricentis.demowebshop.pages.ComputerPage;
+import com.tricentis.demowebshop.pages.DesktopsPage;
+import com.tricentis.demowebshop.pages.HomePage;
+import com.tricentis.demowebshop.testbase.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import resources.testdata.TestData;
+
+@Listeners(CustomListeners.class)
+public class ComputerPageTest extends BaseTest {
+
+    HomePage homePage;
+    ComputerPage computerPage;
+    DesktopsPage desktopsPage;
+    BuildYourOwnComputerPage buildYourOwnComputer;
+
+
+
+    @BeforeMethod(alwaysRun = true)
+    public void inIt() {
+        homePage = new HomePage();
+        computerPage = new ComputerPage();
+        desktopsPage = new DesktopsPage();
+        buildYourOwnComputer = new BuildYourOwnComputerPage();
+
+    }
+
+
+
+    @Test(groups = {"sanity"})
+    public void verifyUserShouldNavigateToComputerPageSuccessfully(){
+
+
+
+
+        //Click on the COMPUTER tab
+        homePage.clickOnComputer();
+
+
+        //Verify "Computer" text
+        Assert.assertEquals(computerPage.getComputerMessageRequired(), "Computers");
+
+
+    }
+    @Test(groups = {"sanity", "smoke"})
+    public void verifyUserShouldNavigateToDesktopsPageSuccessfully(){
+
+        //Click on the COMPUTER tab
+        homePage.clickOnComputer();
+
+        //Click on the Desktops link
+        computerPage.clickOnDesktop();
+
+
+        //Verify "Desktops" text
+        Assert.assertEquals(desktopsPage.getDesktopText(), "Desktops");
+
+    }
+
+    @Test(groups = {"smoke", "regression1"}, dataProvider = "computerData", dataProviderClass = TestData.class)
+    public void verifyThatUserShouldBuildYouOwnComputerAndAddThemToCartSuccessfully(String processor, String ram, String hdd, String os, String software){
+
+
+        //Click on the COMPUTER tab
+        homePage.clickOnComputer();
+
+        //Click on the Desktops link
+        computerPage.clickOnDesktop();
+
+
+        //Click on the product name "Build your own computer"
+        desktopsPage.clickOnBuildComputerButton();
+
+        //Select processor "processor"
+        buildYourOwnComputer.selectProcessorFromDropdown(processor);
+
+        //Select RAM "ram"
+        buildYourOwnComputer.selectRam(ram);
+
+        //Select HDD "hdd"
+        buildYourOwnComputer.selectHddRadio(hdd);
+
+        //Select OS "os"
+        buildYourOwnComputer.selectOs(os);
+
+        //Select Software "software"
+        buildYourOwnComputer.selectCheckBox(software);
+
+
+        //Click on "ADD TO CART" Button
+        buildYourOwnComputer.clickOnAddToCart();
+
+
+//        //Verify the message "The product has been added to your shopping cart"
+//      Assert.assertEquals(buildYourOwnComputer.getDesktopText(), "The product has been added to your shopping cart");
+
+
+    }
+}
---
+++ src/test/java/com/tricentis/demowebshop/testsuite/LoginPageTest.java
@@ -0,0 +1,126 @@
+package com.tricentis.demowebshop.testsuite;
+
+import com.tricentis.demowebshop.customlisteners.CustomListeners;
+import com.tricentis.demowebshop.pages.HomePage;
+import com.tricentis.demowebshop.pages.LoginPage;
+import com.tricentis.demowebshop.testbase.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+@Listeners(CustomListeners.class)
+public class LoginPageTest extends BaseTest {
+
+    HomePage homePage;
+    LoginPage loginPage;
+
+
+    @BeforeMethod(alwaysRun = true)
+    public void inIt() {
+        homePage = new HomePage();
+        loginPage = new LoginPage();
+
+    }
+
+
+
+
+    @Test(groups = {"sanity"})
+    public void userShouldNavigateToLoginPageSuccessFully() throws InterruptedException {
+
+        Thread.sleep(3000);
+
+        //Click on the login link
+        homePage.clickOnLogin();
+
+        //verify that the "Welcome, Please Sign In!" message display
+        Assert.assertEquals(loginPage.getWelcomeText(), "Welcome, Please Sign In!");
+
+    }
+
+    @Test(groups = {"sanity", "smoke"})
+    public void verifyTheErrorMessageWithInValidCredentials() throws InterruptedException {
+
+        Thread.sleep(3000);
+
+        //Click on the login link
+        homePage.clickOnLogin();
+
+
+        //Enter EmailId
+        loginPage.enterEmailId("Pinal456@gmail.com");
+
+
+        //Enter Password
+        loginPage.enterPassword("pinal456");
+
+
+        //Click on the Login Button
+        loginPage.clickOnLoginButton();
+
+
+        //Verify that the Error message
+        String expectedErrorMessage = "Login was unsuccessful. Please correct the errors and try again.\n"
+                + "No customer account found";
+        String actualErrorMessage = loginPage.getErrorMessage();
+        Assert.assertEquals( expectedErrorMessage, actualErrorMessage,"Error message not displayed");
+
+    }
+    @Test(groups = {"smoke", "regression"})
+    public void verifyThatUserShouldLogInSuccessFullyWithValidCredentials() throws InterruptedException {
+
+        Thread.sleep(3000);
+
+        //Click on the login link
+        homePage.clickOnLogin();
+
+
+        //Enter EmailId
+        loginPage.enterEmailId("pinal111@gmail.com");
+
+
+        //Enter Password
+        loginPage.enterPassword("pinal111");
+
+
+        //Click on the Login Button
+        loginPage.clickOnLoginButton();
+
+
+        //Verify that the LogOut link is displayed
+        Assert.assertEquals(loginPage.getLogoutText(), "Log out");
+
+    }
+
+    @Test(groups = {"smoke", "regression1"})
+    public void VerifyThatUserShouldLogOutSuccessFully() throws InterruptedException {
+
+        Thread.sleep(3000);
+
+        //Click on the login link
+        homePage.clickOnLogin();
+
+
+        //Enter EmailId
+        loginPage.enterEmailId("pinal111@gmail.com");
+
+
+        //Enter Password
+        loginPage.enterPassword("pinal111");
+
+
+        //Click on the Login Button
+        loginPage.clickOnLoginButton();
+
+
+        //Verify that the LogOut link is displayed
+        Assert.assertEquals(loginPage.getLogoutText(), "Log out");
+
+        //Verify that the LogIn Link Display
+        Assert.assertEquals(homePage.getLoginText(), "Log in");
+
+
+    }
+
+}
